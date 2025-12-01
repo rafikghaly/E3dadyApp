ca539746c21936c84a1de070ffd9698ace661940
Rafik Ghaly <40712708+rafikghaly@users.noreply.github.com>
2025-12-01 10:14:25 +0200

    Update API_URL to the actual Apps Script URL

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Replace this with your actual Apps Script Web App URL
-const API_URL = 'YOUR_APPS_SCRIPT_URL_HERE';
+const API_URL = 'https://script.google.com/macros/s/AKfycbz1n0-H-x6TesQ0Akc86btR7kMKLzTfZe-P8JC5MpcobSfH1Fdr4pjC33vLlNz3YHICow/exec';
 
 async function apiCall(action, data) {
     const response = await fetch(API_URL, {
@@ -14,4 +14,5 @@ async function apiCall(action, data) {
     });
     
     return await response.json();
+
 }
